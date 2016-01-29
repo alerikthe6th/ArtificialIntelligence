b434f28d10fdf0ef6594c58c8ac32386270e573a
alerikthesixth <alerikvi13@augustana.edu>
2016-01-29 15:42:30 -0600

    adding start files

---
+++ RobotStuff/src/HelloWorld.java
@@ -0,0 +1,30 @@
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.Font;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.utility.Delay;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        GraphicsLCD lcd = BrickFinder.getDefault().getGraphicsLCD();
+        final int SW = lcd.getWidth();
+        final int SH = lcd.getHeight();
+        Button.LEDPattern(4);
+        Sound.beepSequenceUp();
+        
+        lcd.setFont(Font.getLargeFont());
+        lcd.drawString("whoa, there.", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
+        Button.LEDPattern(3);
+        Delay.msDelay(4000);
+        Button.LEDPattern(5);
+        lcd.clear();
+        lcd.refresh();
+        Sound.beepSequence();
+        Delay.msDelay(500);
+        Button.LEDPattern(0);
+    }
+}
