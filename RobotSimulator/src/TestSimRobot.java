a28dd427d84579ac7bb83bc2518b47d6e8c344ab
Alerik Vi <alerikvi13@augustana.edu>
2016-02-13 17:22:34 -0600

    update to testsimrobot.java

--- RobotSimulator/src/TestSimRobot.java
+++ RobotSimulator/src/TestSimRobot.java
@@ -42,8 +42,55 @@ public class TestSimRobot {
 		
 		int x = 0;
 		int y = 0;
+		
+		boolean right = false;
+		boolean left = false;
+		boolean back = false;
+		boolean front = false;
+		
+		
 		CellData[][] map = new CellData[4][4];
 		
+		
+		for (int robotMoveNum = 0; robotMoveNum < 20; robotMoveNum++)	{
+			
+			float distStraight = simRobot.getDistanceMeasurement();
+			simRobot.neckRight90();
+			float distRight = simRobot.getDistanceMeasurement();
+			simRobot.neckRight90();
+			float distBack = simRobot.getDistanceMeasurement();
+			simRobot.neckRight90();
+			float distLeft = simRobot.getDistanceMeasurement();
+			
+			System.out.print("Distances Sensed:  R: " + distRight + " S: " + distStraight + " L:" + distLeft + "B:" + distBack);
+			simRobot.neckRight90();
+			Thread.sleep(2000);
+			
+			if(distRight == .56) {
+				right = true;
+			}
+			if(distLeft == .56) {
+				left = true;
+			}
+			if(distStraight == .56) {
+				front = true;
+			}
+			if(distBack == .56) {
+				back = true;
+			}
+			
+			map[x][y] = new CellData(x,y, front, back, right, left); //saves its location and possibilities
+
+			if (distStraight > 1) {
+				simRobot.forwardOneCell();	
+				if (simRobot.colorSensorSeesGoal()) {
+					System.out.println("FOUND GOAL!");
+					break;
+				}
+			} else  {
+				simRobot.right90();  //turns right when it can't go straight.
+			}
+		}
 		
 		
 	
